cf936c08def671e5d1d8b40a9b7b3891b55dc4fc
jdfranco97 <46454995+jdfranco97@users.noreply.github.com>
2021-02-10 12:03:43 -0600

    Password change fix

--- portfolio/views.py
+++ portfolio/views.py
@@ -172,7 +172,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
+            return redirect('portfolio:home')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
